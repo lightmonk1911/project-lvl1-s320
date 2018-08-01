8ee59eb3d31a80d173e7954c6a09892cb64f29fb
Babenkov Igor <lightmonk1911@gmail.com>
2018-08-01 19:09:16 +0400

    added return from askname,  added bin brain-even

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import readlineSync from 'readline-sync';
 const askName = () => {
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
+  return playerName;
 };
 export default askName;
