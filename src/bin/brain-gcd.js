a084039d195d75a64666bdcfca20f453bcad5d1f
Babenkov Igor <lightmonk1911@gmail.com>
2018-08-01 23:15:07 +0400

    fixed files order, uploaded to npm with gcd

--- src/bin/brain-gcd.js
+++ src/bin/brain-gcd.js
@@ -1,27 +1,9 @@
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
+import { askName, hello } from '..';
+import game from '../games/threeRoundAnswerGame';
+import round from '../rounds/brainGcdRound';
 
-const gcd = (a, b) => {
-  const isCommonDivisor = (div, x = a, y = b) => x % div === 0 && y % div === 0;
-  const [min] = [a, b].sort();
-  let i = 1;
-  while ((min % i !== 0) || !isCommonDivisor(min / i)) {
-    i += 1;
-  }
-  return min / i;
-};
-
-export default (success, fail) => {
-  const random = (min = 0, max = 100) => Math.floor(Math.random() * max - min + 1);
-  const a = random();
-  const b = random();
-
-  const question = `${a} ${b}`;
-  const correctAnswer = gcd(a, b);
-  const answer = readlineSync.question(`Question: ${question}\n`);
-  if (answer === correctAnswer) {
-    success(answer, correctAnswer);
-    return true;
-  }
-  fail(answer, correctAnswer);
-  return false;
-};
+hello();
+console.log('Find the greatest common divisor of given numbers.');
+const playerName = askName();
+game(playerName, round);
