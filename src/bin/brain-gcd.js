fcd6cba4a8257b7a7923776e77c7b8a8c040284b
Babenkov Igor <lightmonk1911@gmail.com>
2018-08-01 23:00:00 +0400

    added bin, logic of brain-gcd

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+const gcd = (a, b) => {
+  const isCommonDivisor = (div, x = a, y = b) => x % div === 0 && y % div === 0;
+  const [min] = [a, b].sort();
+  let i = 1;
+  while ((min % i !== 0) || !isCommonDivisor(min / i)) {
+    i += 1;
+  }
+  return min / i;
+};
+
+export default (success, fail) => {
+  const random = (min = 0, max = 100) => Math.floor(Math.random() * max - min + 1);
+  const a = random();
+  const b = random();
+
+  const question = `${a} ${b}`;
+  const correctAnswer = gcd(a, b);
+  const answer = readlineSync.question(`Question: ${question}\n`);
+  if (answer === correctAnswer) {
+    success(answer, correctAnswer);
+    return true;
+  }
+  fail(answer, correctAnswer);
+  return false;
+};
