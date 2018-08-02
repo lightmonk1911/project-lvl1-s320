2d44f0e8d953817c550b060d240dbe68ab5088c8
Igor <lightmonk1911@gmail.com>
2018-08-02 20:13:30 +0400

    fixed small and big in balance

--- src/games/brainBalance.js
+++ src/games/brainBalance.js
@@ -8,11 +8,13 @@ const getQestionAndAnswer = () => {
   const arrayOfDigits = Array.from(number.toString()).map(digit => Number(digit));
   const sum = arrayOfDigits.reduce((acc, digit) => digit + acc, 0);
   const average = sum / arrayOfDigits.length;
+  const small = Math.floor(average);
+  const big = small + 1;
   const countOfBig = sum % average;
   const countOfSmall = (sum - countOfBig) / average;
   const newArray = [];
   for (let i = 0; i < arrayOfDigits.length; i += 1) {
-    newArray.push(i < countOfSmall ? average : average + 1);
+    newArray.push(i < countOfSmall ? small : big);
   }
   return [arrayOfDigits.join(''), newArray.join('')];
 };
