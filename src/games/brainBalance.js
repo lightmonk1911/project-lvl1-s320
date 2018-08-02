45b0c3978e3a0a6cf07841bc3136b5949298ab0b
Igor <lightmonk1911@gmail.com>
2018-08-02 20:08:20 +0400

    made logic of brain-balance

---
+++ src/games/brainBalance.js
@@ -0,0 +1,20 @@
+import answerGame from '../engine';
+import generateNumber from '../utils';
+
+const description = 'Balance the given number.';
+
+const getQestionAndAnswer = () => {
+  const number = generateNumber(0, 1000);
+  const arrayOfDigits = Array.from(number.toString()).map(digit => Number(digit));
+  const sum = arrayOfDigits.reduce((acc, digit) => digit + acc, 0);
+  const average = sum / arrayOfDigits.length;
+  const countOfBig = sum % average;
+  const countOfSmall = (sum - countOfBig) / average;
+  const newArray = [];
+  for (let i = 0; i < arrayOfDigits.length; i += 1) {
+    newArray.push(i < countOfSmall ? average : average + 1);
+  }
+  return [arrayOfDigits.join(''), newArray.join('')];
+};
+
+export default () => answerGame(description, getQestionAndAnswer);
