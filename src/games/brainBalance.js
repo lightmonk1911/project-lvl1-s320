5a0d81ab4889efff267e5c2c25c313a627101314
Babenkov Igor <lightmonk1911@gmail.com>
2018-08-02 23:23:20 +0400

    fixed bug with brain-balance

--- src/games/brainBalance.js
+++ src/games/brainBalance.js
@@ -10,8 +10,8 @@ const getQestionAndAnswer = () => {
   const average = sum / arrayOfDigits.length;
   const small = Math.floor(average);
   const big = small + 1;
-  const countOfBig = sum % average;
-  const countOfSmall = (sum - countOfBig) / average;
+  const countOfBig = sum % small;
+  const countOfSmall = arrayOfDigits.length - countOfBig;
   const newArray = [];
   for (let i = 0; i < arrayOfDigits.length; i += 1) {
     newArray.push(i < countOfSmall ? small : big);
