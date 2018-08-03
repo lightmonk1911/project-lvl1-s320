1fc816aa86417d072409cef0ef653003ae7b03f9
Igor <lightmonk1911@gmail.com>
2018-08-03 11:54:36 +0400

    fixed isprime

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -4,7 +4,7 @@ import generateNumber from '../utils';
 const description = 'Is this number prime? Say "yes" or "no"';
 const isPrime = (number) => {
   let i = 2;
-  while (i < number / i) {
+  while (i <= number / i) {
     if (number % i === 0) return false;
     i += 1;
   }
